fee26b114b3021d2713b8cb46b47da9eeea4cbb2
Saran Jantara-amornporn <saran.j2108@gmail.com>
2021-06-16 21:54:17 +0700

    first commit

---
+++ LmnwAssignmentTests/LmnwAssignmentTests.swift
@@ -0,0 +1,33 @@
+//
+//  LmnwAssignmentTests.swift
+//  LmnwAssignmentTests
+//
+//  Created by KTB_User on 16/6/2564 BE.
+//
+
+import XCTest
+@testable import LmnwAssignment
+
+class LmnwAssignmentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
