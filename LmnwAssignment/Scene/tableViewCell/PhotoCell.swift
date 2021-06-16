67b19cd0de8257541c1209ed18a6207ce4d78400
Saran Jantara-amornporn <saran.j2108@gmail.com>
2021-06-17 01:13:33 +0700

    setup PhotoCell

--- LmnwAssignment/Scene/tableViewCell/PhotoCell.swift
+++ LmnwAssignment/Scene/tableViewCell/PhotoCell.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Kingfisher
 
 class PhotoCell: UITableViewCell {
     @IBOutlet weak var photoView: UIImageView!
@@ -12,5 +13,22 @@ class PhotoCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    func setupCellData(data: PhotoDisplay) {
+        let processor = DownsamplingImageProcessor(size: photoView.bounds.size)
+        let url = URL(string: data.imageUrl)
+        photoView.kf.setImage(
+            with: url,
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(1)),
+                .cacheOriginalImage
+            ])
+        
+        titleLabel.text = data.name
+        descriptionLabel.text = data.description
+        voteCountLabel.text = data.positiveVotes
     }
 }
